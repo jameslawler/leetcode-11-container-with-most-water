73d5d6ad04c1948c8838d5370e58a4e13de21c31
jameslawler <services@jameslawler.com>
2022-02-13 20:45:09 +0100

    First accepted version

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const maxArea = (height) => {
   let largestArea = 0;
+  let largestHeight = 0;
 
   for (let i = 0; i < height.length; i++) {
+    const valueLeft = height[i];
+
+    if (valueLeft < largestHeight) {
+      continue;
+    }
+
     for (let j = i + 1; j < height.length; j++) {
-      const valueLeft = height[i];
       const valueRight = height[j];
       const minimumHeight = Math.min(valueLeft, valueRight);
       const distance = j - i;
       const area = minimumHeight * distance;
+
+      if (valueLeft > largestHeight) {
+        largestHeight = valueLeft;
+      }
 
       if (area > largestArea) {
         largestArea = area;
